5b1c2d6e65249602e84eb75d8fcd99c341f0d4a9
Mighty Weaver <1874240442@qq.com>
2020-11-01 18:25:27 +0800

    Initial Project Setup

---
+++ BERT_embedding.py
@@ -0,0 +1,16 @@
+import matplotlib
+import torch
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+from pytorch_pretrained_bert import BertTokenizer, BertConfig
+from pytorch_pretrained_bert import BertAdam, BertForSequenceClassification
+from tqdm import tqdm, trange
+import pandas as pd
+import io
+import numpy as np
+import matplotlib.pyplot as plt
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+n_gpu = torch.cuda.device_count()
+torch.cuda.get_device_name(0)
