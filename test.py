92d3c74cf7c13c84531b2a446e44c49c7e54f17a
Mighty Weaver <1874240442@qq.com>
2020-11-05 00:55:38 +0800

    BertTokenizer Encode() Used Framework Redesigned

--- test.py
+++ test.py
@@ -1,22 +1,11 @@
-import json
 import pickle
-from itertools import chain
 
-import pandas as pd
+from transformers import BertTokenizer
 
-# load the training dict and build the tag <--> index dictionary and add PAD tag into it
-train_dict = pickle.load(open("./data/train.pkl", 'rb'))
-tag_list = list(set(chain(*train_dict["tag_seq"])))
-tag_list.append("PAD")
-tag_to_index_dict = {t: i for i, t in enumerate(tag_list)}
-index_to_tag_dict = {i: t for i, t in enumerate(tag_list)}
-print(index_to_tag_dict)
+tokenizer = BertTokenizer.from_pretrained('bert-base-cased', do_lower_case=False)
+test = pickle.load(open('./data/train.pkl', 'rb'))
+for i in range(len(test['word_seq'])):
+    print(len(tokenizer.encode(test['word_seq'][i])))
 
-data = pickle.load(open('./data/test.pkl', 'rb'))
-answer = pd.read_csv("F:/大三上/COMP4901K/answer_e5_bs2_epoch4.csv", index_col=0)
-
-line = 0
-ans_list = json.loads(answer.loc[line, 'labels'])
-
-print(len(ans_list))
-print(len(data['word_seq'][line]))
+a = [1, 2, 3, 4, 5]
+print(a[1:-1])
