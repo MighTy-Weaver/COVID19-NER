e36613d580cf89abf139598b6e2bb269ddee429a
Mighty Weaver <1874240442@qq.com>
2020-11-08 10:33:19 +0800

    test code update for verify the lstm result length

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-import numpy as np
+import pandas as pd
+import json
 
-print(np.random.rand(1,300)[0])
+answer = pd.read_csv("./final_answers/answer_BS256E100LR0.0015.csv", index_col=0)
+for i in range(len(answer)):
+    if len(json.loads(answer.loc[i, 'labels']))!=128:
+        print("Bad length at {}".format(i))
+    else:
+        pass
+print("end")
