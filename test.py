3784ef0d6a84ebdfab2bba12bbc8e6f7fcc2c3c4
Mighty Weaver <1874240442@qq.com>
2020-11-28 11:24:34 +0800

    file rename

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import json
 
 import pandas as pd
 
-answer = pd.read_csv("test_preds.csv", index_col=0)
+answer = pd.read_csv("preds.csv", index_col=0)
 for i in range(len(answer)):
     if len(json.loads(answer.loc[i, 'labels'])) != 128:
         print("Bad length at {}".format(i))
